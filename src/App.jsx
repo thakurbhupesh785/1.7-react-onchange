ad6d2a7a42a492676e696f0f5c4cf95aa56bfe01
Bhupesh-Thakur <153527966+thakurbhupesh785@users.noreply.github.com>
2024-02-10 19:07:26 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// onchange = event handler used primarily with form elements
+// eg- <input>,<textarea>,<select>,<radio>
+// triggers a function every time the value of input changes
+
+import MyComponent from "./MyComponent.jsx"
+
+function App() {
+
+  
+  return(
+   <>
+   <MyComponent/>
+   </>
+  )
+ 
+}
+
+export default App
